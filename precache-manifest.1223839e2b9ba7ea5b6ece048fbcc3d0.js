1a620f0ee45abf494801a183228625fcf211de9b
hai-nguyen1112 <hainguyen871112@gmail.com>
2020-10-12 13:34:07 -0400

    Updates

---
+++ precache-manifest.1223839e2b9ba7ea5b6ece048fbcc3d0.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af4a1727f8e187af2fb8825a881c88c",
+    "url": "/hai-nguyen/index.html"
+  },
+  {
+    "revision": "e6d5496bbe1e57f9189f",
+    "url": "/hai-nguyen/static/css/main.704577d0.chunk.css"
+  },
+  {
+    "revision": "8e8e5599aa1471610cf5",
+    "url": "/hai-nguyen/static/js/2.1f718a2e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/hai-nguyen/static/js/2.1f718a2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6d5496bbe1e57f9189f",
+    "url": "/hai-nguyen/static/js/main.4982a868.chunk.js"
+  },
+  {
+    "revision": "55a00dcba4209a7e54e5",
+    "url": "/hai-nguyen/static/js/runtime-main.d0340237.js"
+  },
+  {
+    "revision": "86785ab361d9ef3e8fe4726c560f072d",
+    "url": "/hai-nguyen/static/media/about.86785ab3.jpg"
+  },
+  {
+    "revision": "13d8dcf5f9d81de0dae4db7d954edb19",
+    "url": "/hai-nguyen/static/media/cfsa.13d8dcf5.jpg"
+  },
+  {
+    "revision": "5018a76b97460a4b505cb980cd28f5f5",
+    "url": "/hai-nguyen/static/media/enzyme.5018a76b.png"
+  },
+  {
+    "revision": "bd3a7cd2ca0465f43ba25a6918c64b5e",
+    "url": "/hai-nguyen/static/media/express.bd3a7cd2.png"
+  },
+  {
+    "revision": "71f6edbd7607f01b04131ff091af8f16",
+    "url": "/hai-nguyen/static/media/ge.71f6edbd.jpg"
+  },
+  {
+    "revision": "db5105bda8fa83cd77363d123f9b66b8",
+    "url": "/hai-nguyen/static/media/hai-nguyen.db5105bd.jpg"
+  },
+  {
+    "revision": "43bab67fc5c0172c09338f0524cf7f82",
+    "url": "/hai-nguyen/static/media/haiavatar.43bab67f.jpg"
+  },
+  {
+    "revision": "03492720ddf1009be6da002eb1112afd",
+    "url": "/hai-nguyen/static/media/hiant.03492720.jpg"
+  },
+  {
+    "revision": "466c972021323d12420fc20a016f5b28",
+    "url": "/hai-nguyen/static/media/javascript.466c9720.jpg"
+  },
+  {
+    "revision": "50f50ad550b9affb29c5055543bef79b",
+    "url": "/hai-nguyen/static/media/mongodb.50f50ad5.png"
+  },
+  {
+    "revision": "89e2c62ce525052f1e65d965b58df984",
+    "url": "/hai-nguyen/static/media/mongoose.89e2c62c.png"
+  },
+  {
+    "revision": "49eb264a0c3383cfcb8925a5b36cf461",
+    "url": "/hai-nguyen/static/media/nodejs.49eb264a.png"
+  },
+  {
+    "revision": "b4d50f46d227054f63dafac8c75b681c",
+    "url": "/hai-nguyen/static/media/react.b4d50f46.png"
+  },
+  {
+    "revision": "822be8db28621d8661f6fa991f9e0b27",
+    "url": "/hai-nguyen/static/media/redux.822be8db.png"
+  },
+  {
+    "revision": "106ecf4a13e4a73d7af3917998318a2f",
+    "url": "/hai-nguyen/static/media/sass.106ecf4a.png"
+  },
+  {
+    "revision": "fead1d450a49086974fec606e8dbb781",
+    "url": "/hai-nguyen/static/media/typescript.fead1d45.jpg"
+  },
+  {
+    "revision": "9da375a248ac3e03943cfae147c91a54",
+    "url": "/hai-nguyen/static/media/undercooked.9da375a2.jpg"
+  },
+  {
+    "revision": "ac8945f22aded8c83d8d20972e3c30c6",
+    "url": "/hai-nguyen/static/media/web.ac8945f2.png"
+  },
+  {
+    "revision": "578bdd0063361be9021cb7bce4d77b94",
+    "url": "/hai-nguyen/static/media/welcome.578bdd00.jpg"
+  }
+]);
