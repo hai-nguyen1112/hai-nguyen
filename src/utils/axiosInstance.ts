86d6db40ad1d8f37b5c78b0459e0123a1cf72a7a
hai-nguyen1112 <hainguyen871112@gmail.com>
2025-10-02 14:39:46 -0400

    Updated apiUrl

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const apiUrl = 'https://hai-nguyen-api.herokuapp.com';
+export const apiUrl = 'https://hai-nguyen-api.onrender.com';
 
 export const axiosInstance = axios.create({
   baseURL: `${apiUrl}/api/v1`,
